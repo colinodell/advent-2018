36da9b47f14afe3bfd3a34540e2dda6c571dd687
Colin O'Dell <colinodell@gmail.com>
2018-12-01 07:37:15 -0500

    Prepare repo for 2018 challenges

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "env": {
+    "browser": false,
+    "es6": true,
+    "mocha": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "rules": {
+    "indent": ["error", 2],
+    "quotes": ["error", "single"],
+    "semi": 2
+  }
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# advent-2018
+
+My solutions to adventofcode.com 2018 challenges! This year I'll be using NodeJS.
